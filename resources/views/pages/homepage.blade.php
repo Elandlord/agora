2d28c03318df4bd6b6dc1396f581495924031566
Eric Landheer <Eric Landheer>
2017-01-10 12:06:48 +0100

    1st try navbar agora

---
+++ resources/views/pages/homepage.blade.php
@@ -0,0 +1,39 @@
+@extends('master')
+
+@section('title')
+Homepage
+@stop
+
+@section('content')
+
+<div class='container-fluid push-aside-xl bg-main space-inside-md'>
+	<div class='row'>
+		<div class='col-lg-6 text-center'>
+			<h2 class='text-color-light'>AGENDA</h2>
+		</div>
+		<div class='col-lg-6 text-center'>
+			<h2 class='text-color-light'>NIEUWS</h2>
+		</div>
+	</div>
+</div>
+
+<div class='container-fluid push-aside-xl bg-secondary space-inside-md'>
+	<div class='row'>
+		<div class='col-lg-6'>
+			<div class='row'>
+				<div class='col-lg-2 bg-accent'>
+					<h2 class="text-color-light">Za <br/>3 DEC</h2>
+				</div>
+
+				<div class='col-lg-10'>
+				<h3 class="text-color-light">14:00 <br/> UUR</h3>
+				</div>
+			</div>
+		</div>
+		<div class='col-lg-6'>
+			
+		</div>
+	</div>
+</div>
+
+@stop
---
+++ resources/views/partials/navigation.blade.php
@@ -0,0 +1,75 @@
+
+
+	
+<!-- Container voor centreren van content -->
+	
+		<div class="container-fluid bg-main">
+			
+		<!-- Row wrapper voor content -->
+			<div class="row">
+				
+			<!-- Columns voor content indelingen -->
+
+			<!-- navigatie -->
+		
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 placeholder-nav">
+					
+					
+
+					<!-- navbar -->
+							<nav class="navbar navbar-default bg-main space-outside-up-sm"> 
+
+							  <div class="container-fluid">
+							    <!-- Brand and toggle get grouped for better mobile display -->
+							    <div class="navbar-header">
+
+							      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+
+							        <span class="sr-only">Toggle navigation</span>
+
+							        <span class="icon-bar"></span>
+
+							        <span class="icon-bar"></span>
+
+							        <span class="icon-bar"></span>
+
+							      </button>
+							    
+							    </div>
+
+							    <!-- Collect the nav links, forms, and other content for toggling -->
+							    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+							      <ul class="nav navbar-nav">
+
+							        <li class='space-inside-left-xl'><a class='bg-main font-md text-color-light' href="/agenda">AGENDA <span class="sr-only">(current)</span></a></li>
+
+							        <li class='space-inside-left-xl'><a class='bg-main font-md text-color-light' href="/nieuws">NIEUWS</a></li>
+
+							       	<li class='space-inside-left-xl'><a class='bg-main font-md text-color-light' href="/fotos">FOTO'S</a></li>
+
+							       	<li style='padding-left: 120px;'><img src='images/agora-logo.png' style='position:absolute; top:-20px; z-index:10; max-width: 250px;' class='hidden-xs hidden-sm'/></li>
+
+							       	<li style='padding-left: 400px;'><a class='bg-main font-md text-color-light' href="/over-ons">OVER ONS</a></li>
+
+							       	<li class='space-inside-left-xl'><a class='bg-main font-md text-color-light' href="/ruimtes">RUIMTES</a></li>
+
+							       	<li class='space-inside-left-xl'><a class='bg-main font-md text-color-light' href="/contact">CONTACT</a></li>
+
+							      </ul>
+							      
+							    </div><!-- /.navbar-collapse -->
+
+							  </div><!-- /.container-fluid -->
+
+							</nav>
+
+						<!-- end of navbar -->
+					
+				
+				</div>
+
+			</div>
+
+		</div>
+		
